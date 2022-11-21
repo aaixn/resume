aa177ce6fb33a69388fc76d653436c3121987c61
aaixn <axnguyen246@gmail.com>
2022-11-21 13:31:35 -0800

    fix readFile

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const { info } = require('console')
 const fs = require('fs')
 
-fs.readFile("resume/info.json", "utf8", function(err, data) {
+fs.readFile(__dirname + "/info.json", "utf8", function(err, data) {
     if (err) {
         console.log(err);
     } else {
